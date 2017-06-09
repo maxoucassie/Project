1ffae08c9e62a5c8c096581db9debe9e4afd7125
maxoucassie <maxouchobbits@outlook.fr>
2017-06-09 14:09:28 +0200

    First commit

---
+++ js/function.js
@@ -0,0 +1,15 @@
+$(function () {
+
+		$(window).scroll(function () {
+
+		var wScroll = $(this).scrollTop();
+		var menu = $(".menu");
+
+		if(wScroll >= 485) {
+			menu.addClass("bg");
+		}
+		else {
+			menu.removeClass("bg");
+		}
+	});
+});
